293fae469aee5da8cb16c2a5e5658b727732a792
Aditya Sharat <adityasharat@gmail.com>
2014-09-09 12:51:30 +0530

    FCTRL complete

--- easy/FCTRL.js
+++ easy/FCTRL.js
@@ -11,16 +11,32 @@ process.stdin.on('data', function (chunk) {
 });
 
 process.stdin.on('end', function () {
-    stdout.write(run(input) + '\n');
+    stdout.write(run(input));
 });
 
 run = function (input) {
     var lines = input.split('\n'),
-        n = lines[0];
+        n = parseInt(lines[0], 10),
+        index,
+        output = '';
 
-    lines.forEach(function (number, index) {
-        if (index === 0) {
-            return;
+    var sum = 0,
+        zeros = 0,
+        power = 1,
+        number;
+
+    for (index = 1; index < n + 1; index++) {
+        number = lines[index];
+        sum = 0;
+        zeros = 0;
+        power = 1;
+
+        while (zeros = Math.floor(parseInt(number, 10) / Math.pow(5, power))) {
+            sum += zeros;
+            power++;
         }
-    });
+
+        output += sum + '\n';
+    }
+    return output;
 };

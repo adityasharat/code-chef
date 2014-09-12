a89bd94a03d21ab936d0605d2b45319d9fe156e2
Aditya Sharat <adityasharat@gmail.com>
2014-09-12 15:01:26 +0530

    still fails :(

--- easy/INTEST.js
+++ easy/INTEST.js
@@ -1,23 +1,35 @@
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-var i = 0;
-process.stdin.on('data', function (chunk) {
-    var lines = chunk.toString().split('\n'),
-        input = lines[0].split(' '),
-        //n = parseInt(input[0], 10),
-        k = parseInt(input[1], 10);
+var stdin = process.stdin,
+    stdout = process.stdout,
+    input = '',
+    run;
 
-    lines.forEach(function (num, index) {
-        if (index === 0) {
-            return;
-        }
-        if (parseInt(num, 10) % k === 0) {
-            i++;
-        }
-    });
+stdin.setEncoding('utf8');
+stdin.resume();
+
+process.stdin.on('data', function (chunk) {
+    input += chunk;
 });
 
 process.stdin.on('end', function () {
-    process.stdout.write(i + "\n");
+    stdout.write(run(input) + '\n');
     process.exit();
 });
+
+run = function (input) {
+    var lines = input.split('\n'),
+        line0 = lines[0].split(' '),
+        n = parseInt(line0[0], 10),
+        k = parseInt(line0[1], 10),
+        index,
+        i = 0,
+        number;
+
+    for (index = 1; index < n + 1; index++) {
+        number = parseInt(lines[index], 10);
+
+        if (parseInt(number, 10) % k === 0) {
+            i++;
+        }
+    }
+    return i;
+};
